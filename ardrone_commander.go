b231d4c2bb9092ab78b7069b7c314e3e6cb078bb
Adrian Zankich <adrian@hybridgroup.com>
2014-12-06 19:55:16 -0800

    Add configuration file support

--- ardrone_commander.go
+++ ardrone_commander.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"math"
+	"os"
 	"time"
 
 	"github.com/hybridgroup/gobot"
@@ -11,17 +15,30 @@ import (
 	"github.com/hybridgroup/gobot/platforms/gpio"
 )
 
+const CONFIG_FILE = "/data/video/dronedrop.json"
+const VERSION string = "0.3"
+
+var GRAB uint8 = 40
+var LOAD uint8 = 50
+var DROP uint8 = 135
+
+type config struct {
+	Commander bool  `json:"commander"`
+	Grab      uint8 `json:"grab"`
+	Drop      uint8 `json:"drop"`
+	Load      uint8 `json:"load"`
+}
+
 type pair struct {
 	x float64
 	y float64
 }
 
-const GRAB uint8 = 40
-const LOAD uint8 = 50
-const DROP uint8 = 135
-const VERSION string = "0.2"
-
 func main() {
+	c, err := readConfig()
+	if err != nil {
+		log.Println(err)
+	}
 	gbot := gobot.NewGobot()
 
 	server := api.NewAPI(gbot)
@@ -49,6 +66,15 @@ func main() {
 	}
 
 	work := func() {
+		if !c.Commander {
+			for {
+				if errs := ardroneAdaptor.Disconnect(); len(errs) > 0 {
+					log.Println("disconnecting...")
+					break
+				}
+			}
+		}
+
 		go func() {
 			for {
 				pair := leftStick
@@ -163,8 +189,39 @@ func main() {
 		return VERSION
 	})
 
-	gbot.AddCommand("disconnect", func(params map[string]interface{}) interface{} {
-		return ardroneAdaptor.Disconnect()
+	gbot.AddCommand("config", func(params map[string]interface{}) interface{} {
+		if value, ok := params["grab"]; ok {
+			GRAB = uint8(value.(float64))
+			c.Grab = GRAB
+		}
+		if value, ok := params["load"]; ok {
+			LOAD = uint8(value.(float64))
+			c.Load = LOAD
+		}
+		if value, ok := params["drop"]; ok {
+			DROP = uint8(value.(float64))
+			c.Drop = DROP
+		}
+		writeConfig(c)
+		return nil
+	})
+
+	gbot.AddCommand("commander", func(params map[string]interface{}) interface{} {
+		if value, ok := params["enable"]; ok {
+			if !value.(bool) {
+				c.Commander = false
+				writeConfig(c)
+				for {
+					if errs := ardroneAdaptor.Disconnect(); len(errs) > 0 {
+						return nil
+					}
+				}
+			}
+		}
+
+		c.Commander = true
+		writeConfig(c)
+		return nil
 	})
 
 	gbot.AddRobot(robot)
@@ -180,4 +237,32 @@ func validatePitch(value float64) float64 {
 		return 1.0
 	}
 	return 0.0
+}
+
+func readConfig() (c config, err error) {
+	c = config{Commander: true, Grab: 40, Load: 50, Drop: 153}
+
+	if _, err := os.Stat(CONFIG_FILE); err != nil {
+		if err = writeConfig(c); err != nil {
+			return c, err
+		}
+	} else if f, err := os.OpenFile(CONFIG_FILE, os.O_RDWR|os.O_CREATE, 0666); err != nil {
+		return c, err
+	} else {
+		if err := json.NewDecoder(f).Decode(&c); err != nil {
+			return c, err
+		}
+	}
+	return
+}
+
+func writeConfig(c config) (err error) {
+	if buf, err := json.Marshal(c); err != nil {
+		return err
+	} else {
+		if err := ioutil.WriteFile(CONFIG_FILE, buf, 0666); err != nil {
+			return err
+		}
+	}
+	return
 }
